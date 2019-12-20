5ad355f1a0648a690f6f24b64d316d8ef0f1e5dc
Thalles Sobral <thazsobral@gmail.com>
2019-12-20 09:51:49 -0300

    Adicinei a conexão com BD

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+// Iniciando o App
+const app = express()
+
+// Iniciando o DB
+mongoose.connect(
+  "mongodb+srv://user:user@cluster0-gt40f.mongodb.net/test?retryWrites=true&w=majority",
+  { useNewUrlParser: true,
+  useUnifiedTopology: true }
+)
+
+// Rotas
+app.get('/',(req, res) => {
+  res.send('Hello')
+})
+
+app.listen(3001)
