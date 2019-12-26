09887c6a5ac4e2d9ed60f03af1ed1096038d6145
Thalles Sobral <thazsobral@gmail.com>
2019-12-26 14:22:00 -0300

    Completei o CRUD no controller

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,9 @@ const routes = express.Router()
 const ProductController = require('./controllers/ProductController')
 
 routes.get('/', ProductController.index)
+routes.get('/:id', ProductController.show)
 routes.post('/', ProductController.store)
+routes.put('/:id', ProductController.update)
+routes.delete('/:id', ProductController.destroy)
 
 module.exports = routes
